0226cd795a986ba3cc787037b31067735d51a1b3
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2023-07-27 20:09:14 -0300

    Removed unnecessary domain classes

---
+++ grails-app/domain/beautify/AppointmentSchedule.groovy
@@ -0,0 +1,7 @@
+package beautify
+
+class AppointmentSchedule {
+
+    static constraints = {
+    }
+}
---
+++ src/test/groovy/beautify/AppointmentScheduleSpec.groovy
@@ -0,0 +1,18 @@
+package beautify
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class AppointmentScheduleSpec extends Specification implements DomainUnitTest<AppointmentSchedule> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
